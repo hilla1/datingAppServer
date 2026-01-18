47f9dad6a991f9a6bb6c38ec168cb81116d9e298
Kipkirui Hillary <48549414+hilla1@users.noreply.github.com>
2026-01-18 19:46:34 +0300

    Update nodemailer.js

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,20 +1,11 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,          // SSL port (works on Render)
-  secure: true,       // SSL from the start
-  auth: {
-    user: process.env.EMAIL_USER, // Gmail email
-    pass: process.env.EMAIL_PASS, // App password (required if 2FA is enabled)
-  },
-  tls: {
-    rejectUnauthorized: false,   // avoids SSL issues on cloud hosts
-  },
-  connectionTimeout: 20000,       // 20s timeout
-  greetingTimeout: 20000,
-  socketTimeout: 20000,
-  debug: process.env.NODE_ENV !== "production",
+    service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
 });
 
 export default transporter;
